a1b08b5beaae1798c97d95d82cbc744012812f0a
Matteus Varlesse <varlesse04@gmail.com>
2024-02-13 22:35:03 -0300

    chore: change name

--- src/app.tsx
+++ src/app.tsx
@@ -61,12 +61,10 @@ export function App() {
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
-      <img src={logo} alt="NLW Expert" />
-
       <form className="w-full">
         <input
           type="text"
-          placeholder="Busque em suas notas..."
+          placeholder="Busque por palavras em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
           onChange={handleSearch}
         />
